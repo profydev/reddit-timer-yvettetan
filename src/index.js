ae2b08b7f4e3a61619cdbcf8f7fc02514a7dd15f
yvettetan <yvettetan97@gmail.com>
2022-03-21 22:54:41 +0800

    Install styled-components and styled-normalize

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Normalize } from 'styled-normalize';
 import App from './App';
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <>
+    <Normalize />
+    <App />
+  </>,
+  document.getElementById('root'),
+);
