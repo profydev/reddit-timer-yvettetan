9228618729a49da86596de8e595617a1212c12a4
yvettetan <yvettetan97@gmail.com>
2022-03-21 23:28:12 +0800

    Add global styles

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Normalize } from 'styled-normalize';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+import theme from './theme';
+import GlobalCSS from './globalStyle';
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(
   <>
-    <Normalize />
-    <App />
+    <ThemeProvider theme={theme}>
+      <Normalize />
+      <GlobalCSS />
+      <App />
+    </ThemeProvider>
   </>,
   document.getElementById('root'),
 );
